4bd853fa3f6123afb15c6d02b7caf7c4faba26c3
Ifaz Aiman <ifazzaiman17@gmail.com>
2024-12-30 19:03:23 +0600

    fix login page

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,6 @@
 import Folder from "./folder.svg?react";
-
+import Google from "./google.svg?react";
+import Apple  from "./apple.svg?react";
 export const SvgFolder = Folder;
+export const SvgGoogle = Google;
+export const SvgApple = Apple; 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { z } from "zod";
-import LoginBackground from "../assets/images/login-background.png";
-import axios from "axios";
 import axiosInstance from "../api/axiosInstance";
+import LoginBackground from "../assets/images/login.png";
+import { SvgApple, SvgGoogle } from "../assets/svg";
 
 const emailRegex = /^[a-zA-Z]+[a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
@@ -26,13 +26,11 @@ const Login = () => {
 
     try {
       const validatedData = loginSchema.parse({ email, password });
-
       console.log("Login details:", validatedData);
 
       setErrors({});
 
       const response = await axiosInstance.post("/user/login", validatedData);
-
       console.log("Login successful:", response.data);
       alert("Login successful!");
     } catch (error: unknown) {
@@ -46,9 +44,6 @@ const Login = () => {
           }
         });
         setErrors(errorMessages);
-      } else if (axios.isAxiosError(error)) {
-        console.error("API error:", error.response?.data || error.message);
-        alert("Login failed. Please check your credentials.");
       } else {
         console.error("Unexpected error:", error);
         alert("An unexpected error occurred.");
@@ -57,69 +52,68 @@ const Login = () => {
   };
 
   return (
-    <div className="flex min-h-screen">
-      <div className="flex-1 relative h-screen">
-        <img src={LoginBackground} alt="Login Background" className="w-full h-full object-cover opacity-90" />
+    <div className="min-h-screen flex flex-col">
+      <div className="w-full h-[50vh] bg-cover">
+        <img src={LoginBackground} alt="Login Background" className="w-full h-full object-fill object-center opacity-90" />
       </div>
-
-      <div className="flex-1 flex justify-center items-center bg-white p-6">
-        <div className="w-full max-w-md bg-white p-10 rounded-xl shadow-sm">
-          <div className="absolute top-4 right-3">
-            <p>
-              Not a member?
-              <a href="/register" className="text-sm text-blue-500 hover:underline pl-2">
-                Register now
-              </a>
-            </p>
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg mt-[-45px]">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold text-blue-900 mb-9">Log In to Sultan</h1>
           </div>
-          <h1 className="text-3xl font-semibold mb-4 text-gray-800 text-center">Hello Again!</h1>
-          <p className="text-sm text-gray-500 mb-6 text-center">Welcome back you have been missed!</p>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-5 text-left">
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="name@gmail.com"
-                className="w-full mt-2 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
-              {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-            </div>
-            <div className="mb-6 text-left">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter your password"
-                className="w-full mt-2 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
-              {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
-            </div>
-            <div className="mb-6 text-right">
-              <Link to="reset-password" className="text-sm text-blue-500 hover:underline">
-                Forget Password?
-              </Link>
-            </div>
-            <button type="submit" className="w-full p-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg">
-              Login
+          <div className="space-y-6 mt-4">
+            <button className="w-full py-3 bg-white border border-blue-500 text-blue-400 font-medium rounded-lg flex items-center justify-center hover:bg-gray-200 gap-x-3">
+              <SvgGoogle /> Continue with Google
             </button>
-          </form>
-          <div className="relative flex items-center my-10">
-            <div className="flex-grow border-t border-gray-300"></div>
-            <span className="mx-4 text-sm text-gray-500">Or continue with</span>
-            <div className="flex-grow border-t border-gray-300"></div>
+            <button className="w-full py-3 bg-white border border-blue-500 text-blue-400 font-medium rounded-lg flex items-center justify-center hover:bg-gray-200 gap-x-3">
+              <SvgApple /> Continue with Apple
+            </button>
           </div>
-          <div className="mt-6 flex justify-center">
-            <i className="fa-brands fa-google cursor-pointer text-3xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></i>
-            <i className="fa-brands fa-twitter cursor-pointer text-3xl text-sky-500 pl-5"></i>
+          {/*
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="name@example.com"
+              className="mt-2 block w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500"
+            />
+            {errors.email && <p className="text-red-500 text-sm mt-2">{errors.email}</p>}
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="mt-2 block w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500"
+            />
+            {errors.password && <p className="text-red-500 text-sm mt-2">{errors.password}</p>}
+          </div>
+          <button
+            type="submit"
+            className="w-full py-3 bg-yellow-500 text-white font-medium rounded-lg shadow-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+          >
+            Log In
+          </button> 
+        </form> */}
+          <div className="text-center my-6 text-sm text-black-500">Or</div>
+          <div className="text-center">
+            <Link
+              to="/register"
+              className="w-full py-3 bg-blue-500 text-white font-medium rounded-lg flex items-center justify-center hover:bg-blue-600"
+            >
+              Create Account
+            </Link>
           </div>
         </div>
       </div>
